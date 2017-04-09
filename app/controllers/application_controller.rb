06d69fab71bbcc4bf3ec621262cbc1a8c91fced3
hal13 <s9813180@gmail.com>
2017-04-09 10:22:25 +0900

    Finished Chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,10 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from Exception, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
+  include ErrorHandlers if Rails.env.production?
 
   private
+
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
@@ -20,14 +19,5 @@ class ApplicationController < ActionController::Base
       'customer'
     end
   end
-
-  def rescue500(e)
-    @exception = e
-    render 'errors/internal_server_error', status: 500
-  end
-
-  def rescue403(e)
-    @exception = e
-    render 'errors/forbidden', status: 403
-  end
+  
 end
